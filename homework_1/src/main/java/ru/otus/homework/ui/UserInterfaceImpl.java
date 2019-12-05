64bf7eb4fb23fd2425cf8fe447eeaecd66df8f01
Radrek <radrek@mail.ru>
2019-12-06 01:26:39 +0700

    feat(UI): add UI

---
+++ homework_1/src/main/java/ru/otus/homework/ui/UserInterfaceImpl.java
@@ -0,0 +1,134 @@
+package ru.otus.homework.ui;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ru.otus.homework.controller.checker.CheckerController;
+import ru.otus.homework.controller.question.QuestionController;
+import ru.otus.homework.dto.AnswerDto;
+import ru.otus.homework.dto.QuestionDto;
+import ru.otus.homework.dto.ResultDto;
+import ru.otus.homework.ui.questuion.QuestionType;
+import ru.otus.homework.ui.user.User;
+import ru.otus.homework.ui.util.ConsoleUtils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Scanner;
+
+public class UserInterfaceImpl implements UserInterface {
+    private static final Logger LOGGER = LogManager.getLogger(UserInterfaceImpl.class);
+
+    private final QuestionController questionController;
+    private final CheckerController checkerController;
+    private final ConsoleUtils consoleUtils;
+    private final Scanner in;
+
+    public UserInterfaceImpl(QuestionController questionController, CheckerController checkerController,
+                             ConsoleUtils consoleUtils) {
+        this.questionController = questionController;
+        this.checkerController = checkerController;
+        this.consoleUtils = consoleUtils;
+        this.in = new Scanner(System.in);
+    }
+
+    @Override
+    public void start() {
+        showGreeting();
+        User user = getUser();
+        List<QuestionDto> questions = questionController.getQuestions();
+        List<AnswerDto> answers = getAnswers(questions);
+        List<ResultDto> resultDtos = checkerController.checkAnswersOnCorrect(answers);
+        System.out.println(
+                String.format("%s %s ваши результаты ответов представлены ниже: ",
+                        user.getFirstName(), user.getSecondName()));
+        resultDtos
+                .forEach(resultDto -> {
+                    String result;
+                    switch (resultDto.getStatus()) {
+                        case OK:
+                            result = "Правильно";
+                            break;
+                        case FAIL:
+                            result = "Неправильно";
+                            break;
+                        case QUESTION_NOT_FOUND:
+                        default:
+                            result = "Вопрос не был найден";
+                            break;
+                    }
+                    System.out.println(String.format("%d. %s", resultDto.getNumber(), result));
+                });
+    }
+
+    private void showGreeting() {
+        System.out.println("Hello my friend.");
+    }
+
+    private User getUser() {
+        String firstName = getPartName("first");
+        String secondName = getPartName("second");
+        return new User(firstName, secondName);
+    }
+
+    private String getPartName(String part) {
+        String partNameQuestion = String.format("Write your %s name: ", part);
+        return consoleUtils.askUserAndGetNotBlankAnswer(partNameQuestion, in);
+    }
+
+    private List<AnswerDto> getAnswers(List<QuestionDto> questions) {
+        List<AnswerDto> answers = new ArrayList<>();
+        for (QuestionDto question : questions) {
+            if (QuestionType.MULTIPLE_CHOICE.name().equals(question.getQuestionType().name())) {
+                if (CollectionUtils.isEmpty(question.getAnswers())) {
+                    LOGGER.warn("Question has no answers with number = {} and type {}",
+                            question.getNumber(), question.getQuestionType());
+                    continue;
+                }
+                answers.add(getUserAnswer(question));
+            }
+        }
+        return answers;
+    }
+
+    private AnswerDto getUserAnswer(QuestionDto question) {
+        askQuestion(String.format("%d. %s", question.getNumber(), question.getQuestion()));
+        showAnswers(question.getAnswers());
+        String userAnswer = getUserAnswer(question.getAnswers());
+        return new AnswerDto(question.getNumber(), userAnswer);
+    }
+
+    private void askQuestion(String question) {
+        System.out.println(question);
+    }
+
+    private void showAnswers(List<String> answers) {
+        System.out.println("Ниже представлены варианты ответов: ");
+        answers.forEach(answer ->
+                System.out.println(String.format("\t-- %s", answer)));
+    }
+
+    private String getUserAnswer(Collection<String> answers) {
+        String userAnswer;
+        while (true) {
+            userAnswer = consoleUtils.askUserAndGetNotBlankAnswer("Ответ: ", in);
+            if (matchUserAnswer(answers, userAnswer)) {
+                return userAnswer;
+            } else {
+                System.out.println("Нет совпадений, попробуйте еще раз!");
+            }
+        }
+    }
+
+    private boolean matchUserAnswer(Collection<String> answers, String userAnswer) {
+        boolean result;
+        for (String answer : answers) {
+            result = userAnswer.toLowerCase().equals(answer.toLowerCase());
+            if (result) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ homework_1/src/main/java/ru/otus/homework/ui/user/User.java
@@ -0,0 +1,20 @@
+package ru.otus.homework.ui.user;
+
+public class User {
+    private final String firstName;
+
+    private final String secondName;
+
+    public User(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+}
