c1ceb9992e4f4a7e8c9e9f34f30a596885b1fb2f
Radrek <radrek@mail.ru>
2020-04-05 01:40:53 +0700

    test(Authorization, User): fix running maven tests

--- homework_4/src/test/java/ru/otus/homework/service/checker/CheckerServiceIT.java
+++ homework_4/src/test/java/ru/otus/homework/service/checker/CheckerServiceIT.java
@@ -1,11 +1,9 @@
 package ru.otus.homework.service.checker;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.otus.homework.checker.CheckerService;
 import ru.otus.homework.checker.additional.Status;
 import ru.otus.homework.checker.dto.AnswerDto;
@@ -17,7 +15,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @TestPropertySource("/application_test.properties")
 class CheckerServiceIT {
